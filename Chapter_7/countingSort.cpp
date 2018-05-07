7d3a0905d4c0c349f6dbe3ae787cd6cf73e9331a
freesinger <wzy545@vip.qq.com>
2018-05-07 20:49:43 +0800

    modified:   countingSort.cpp

---
+++ Chapter_7/countingSort.cpp
@@ -0,0 +1,36 @@
+/* 计数排序（桶排序）*/
+
+#include <iostream>
+#define MAX 200
+using namespace std;
+
+int main(void)
+{
+    int num, C[MAX];
+
+    cin >> num;
+
+    int *A = (int*)malloc(num * sizeof(int));
+    int *B = (int*)malloc(num * sizeof(int));
+    
+    for (int i = 0; i < num; i++)
+        cin >> A[i];
+
+    for (int i = 0; i < num; i++)
+        C[A[i]]++;
+
+    C[0] = C[0];
+    for (int i = 1; i < num; i++)
+        C[i] = C[i] + C[i - 1];
+
+    for (int i = 0; i < num; i++) {
+        B[C[A[i]] - 1] = A[i];
+        C[A[i]]--;
+    }
+
+    for (int i = 0; i < num; i++)
+        cout << B[i] << ' ';
+    cout << endl;
+
+    return 0;
+}
